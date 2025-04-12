0d366ab64ed6b67cac22de53695d672fed513e99
LIUZihe <zliueo@connect.ust.hk>
2025-04-12 11:06:57 +0800

    10000

--- app.py
+++ app.py
@@ -80,5 +80,5 @@ def osm():
 
 if __name__ == '__main__':
     init_db()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
